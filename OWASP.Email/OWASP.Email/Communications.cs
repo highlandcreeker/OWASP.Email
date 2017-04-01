5886550c562d75b88b11408dae1e29827229ca5b
shovelheadfxe <scottcarnley@gmail.com>
2017-04-01 14:35:57 -0400

    initial commit

---
+++ OWASP.Email/OWASP.Email/Communications.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace OWASP.Email
+{
+	public static class Communications
+	{
+		public static void SendMessage(string recipient, string body)
+		{
+			var msg = new MailMessage();
+
+			msg.Body = body;
+			msg.IsBodyHtml = true;
+
+			using (var client = new SmtpClient("10.10.211.22", 25))
+			{
+				msg.Sender = new MailAddress("owasp@blackbaud.com");
+				msg.From = new MailAddress("owasp@blackbaud.com");
+				msg.ReplyToList.Add(new MailAddress("john.young@blackbaud.com"));
+				msg.Subject = "OWASP Secure Coding Quiz";
+
+				string recipientValue = System.Configuration.ConfigurationManager.AppSettings.Get("Bcc");
+
+				if (recipientValue.Length > 0)
+				{
+					var recipients = recipientValue.Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+
+					foreach (var r in recipients)
+					{
+						try
+						{
+							msg.Bcc.Add(new MailAddress(recipient));
+						}
+						catch (FormatException)
+						{
+							throw new ArgumentException("Invalid email address format for the recipient. Please check the app config file (key=Bcc) for improperly formatted recipients.");
+						}
+					}
+				}
+
+				client.DeliveryFormat = SmtpDeliveryFormat.SevenBit;
+				client.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+#if DEBUG
+				//using (var f = System.IO.File.CreateText(@"D:\Temp\owasp\" + recipient + ".html"))
+				//{
+				//	f.Write(body.ToCharArray());
+				//}
+				msg.To.Add(new MailAddress("scott.carnley@blackbaud.com"));
+				client.Send(msg);
+#else
+			msg.To.Add(new MailAddress(recipient));
+			client.Send(msg);
+#endif
+			}
+		}
+	}
+}
---
+++ OWASP.Email/OWASP.Email/MergeControl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace OWASP.Email
+{
+	public class MergeControl
+	{
+		private static string GetBody()
+		{
+			var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+			var resourceName = "OWASP.Email.Message.Body.txt";
+
+			using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+			using (StreamReader reader = new StreamReader(stream))
+			{
+				return reader.ReadToEnd();
+			}
+		}
+		private static string GetRow()
+		{
+			var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+			var resourceName = "OWASP.Email.Message.Row.txt";
+
+			using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+			using (StreamReader reader = new StreamReader(stream))
+			{
+				return reader.ReadToEnd();
+			}
+		}
+
+		public static string MergeBody(string rows)
+		{
+			string body = GetBody();
+			return body.Replace("$$rows$$", rows);
+		}
+
+		public static string MergeRow(string first, string last, string wdEmail)
+		{
+			string row = GetRow();
+
+			row = row.Replace("$$first$$", first);
+			row = row.Replace("$$last$$", last);
+			row = row.Replace("$$email$$", wdEmail);
+
+			return row;
+		}
+	}
+}
